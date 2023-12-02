025d6f6eaf763323ed646e2f42ad01b85758d6c1
Rudolph Almeida <rudolf1.almeida@gmail.com>
2023-12-03 09:05:09 +1100

    Create window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use glium::Surface;
+use winit::event::{Event, WindowEvent};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = winit::event_loop::EventLoop::new();
+    let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new().with_inner_size(500, 500).with_title("Flappy Birb").build(&event_loop);
+
+    let mut surface = display.draw();
+    surface.clear_color(0.0, 0.0, 1.0, 1.0);
+    surface.finish().unwrap();
+
+    event_loop.run(move |ev, _, control_flow| {
+        match ev {
+            Event::NewEvents(_) => {}
+            Event::WindowEvent { event, .. } => {
+                match event {
+                    WindowEvent::CloseRequested => control_flow.set_exit(),
+                    WindowEvent::KeyboardInput { .. } => {}
+                    _ => {}
+                }
+            }
+            Event::RedrawRequested(_) => {}
+            Event::RedrawEventsCleared => window.request_redraw(),
+            _ => {}
+        }
+    });
 }
