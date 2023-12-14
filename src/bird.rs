6af0f0b5a7f69b5dc68dfccb2d8a2e16aed2d555
Rudolph Almeida <rudolf1.almeida@gmail.com>
2023-12-14 23:34:52 +1100

    Simplify bird movement

--- src/bird.rs
+++ src/bird.rs
@@ -22,9 +22,9 @@ const FLAP_CYCLE: [Flap; 4] = [Flap::Down, Flap::Mid, Flap::Up, Flap::Mid];
 
 pub struct Bird {
     textures: [Texture; 3],
-    position: na::Vector2<f32>,
-    velocity: na::Vector2<f32>,
-    flap: usize,
+    y_position: f32,
+    y_velocity: f32,
+    flap_index: usize,
     flap_duration: Duration,
     rotation: f32,
 }
@@ -47,17 +47,17 @@ impl Bird {
         ];
 
         let (width, height) = display.get_framebuffer_dimensions();
-        let position = na::Vector2::new(width as f32 * 0.25, height as f32 * 0.50);
-        let velocity = na::Vector2::new(0.0, 0.0);
+        let y_position = height as f32 * 0.50;
+        let y_velocity = 0.0;
         let flap = 0;
         let flap_duration = Duration::from_secs_f32(0.0);
         let rotation = 0.0;
 
         Self {
             textures,
-            position,
-            velocity,
-            flap,
+            y_position,
+            y_velocity,
+            flap_index: flap,
             flap_duration,
             rotation,
         }
@@ -67,13 +67,16 @@ impl Bird {
 impl Render for Bird {
     fn render(&self, frame: &mut Frame, renderer: &SpriteRenderer, game_state: &GameState) {
         if matches!(game_state.state, PlayState::Playing) {
-            let (width, height) = self.textures[FLAP_CYCLE[self.flap] as usize].size;
+            let (width, height) = self.textures[FLAP_CYCLE[self.flap_index] as usize].size;
+
             let size = na::Vector2::new(width as f32, height as f32) * 1.5;
+            let position = glm::vec2(game_state.viewport_size.0 as f32 * 0.25, self.y_position);
+
             renderer.render(
                 frame,
-                &self.textures[FLAP_CYCLE[self.flap] as usize],
+                &self.textures[FLAP_CYCLE[self.flap_index] as usize],
                 RenderOptions {
-                    position: self.position,
+                    position,
                     size,
                     rotation: self.rotation,
                     ..RenderOptions::default()
@@ -87,19 +90,16 @@ impl Update for Bird {
     fn update(&mut self, dt: Duration, game_state: &mut GameState) {
         if matches!(game_state.state, PlayState::Playing) {
             if game_state.fly_up {
-                self.rotation = -10.0;
-                self.velocity = glm::vec2(0.0, -100.0);
+                self.y_velocity = -250.0;
+                self.rotation = -20.0;
             } else {
-                self.rotation += 180.0 * dt.as_secs_f32();
-                self.velocity += glm::vec2(0.0, 1.0);
-            }
-            self.rotation = self.rotation.clamp(-30.0, 60.0);
-
-            self.position += self.velocity * dt.as_secs_f32();
+                self.y_velocity += 1.0;
+            };
+            self.y_position += self.y_velocity * dt.as_secs_f32();
 
             self.flap_duration += dt;
             if self.flap_duration >= Duration::from_secs_f32(MAX_FLAP_DURATION) {
-                self.flap = (self.flap + 1) % 4;
+                self.flap_index = (self.flap_index + 1) % 4;
                 self.flap_duration -= Duration::from_secs_f32(MAX_FLAP_DURATION);
             }
         }
